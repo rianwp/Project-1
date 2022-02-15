aefad6e622cc83f4547d1b82df129195de1c8b1d
rianwp <90314741+rianwp@users.noreply.github.com>
2022-02-15 14:05:25 +0700

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -10,7 +10,7 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'rianwp2015'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://alsxwcywlyqvwn:fc2c745bd8053a8931e92c259a6645982b4e973cbf3f8f864a5fd5300ab755a7@ec2-3-225-79-57.compute-1.amazonaws.com:5432/d7lj1lqhf5kjfu'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://alsxwcywlyqvwn:fc2c745bd8053a8931e92c259a6645982b4e973cbf3f8f864a5fd5300ab755a7@ec2-3-225-79-57.compute-1.amazonaws.com:5432/d7lj1lqhf5kjfu'
     app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024
     
     db.init_app(app)
