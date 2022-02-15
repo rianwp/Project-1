7eecc9085a1cb016755f0dbac8e73b7224e5c83f
rianwp <90314741+rianwp@users.noreply.github.com>
2022-02-15 23:25:27 +0700

    Update views.py

--- website/views.py
+++ website/views.py
@@ -7,6 +7,7 @@ from .models import Indexintegrity, Transaksi, User, Databarang
 from . import db
 import json
 from datetime import datetime
+import pytz
 views = Blueprint('views', __name__)
 
 ALLOWED_EXTENSION = set(['png','jpg', 'jpeg'])
@@ -22,7 +23,7 @@ def default():
 @views.route('/dashboard')
 @login_required
 def dashboard():
-    yearnow = datetime.now().strftime("%Y")
+    yearnow = datetime.now(pytz.timezone('Asia/Bangkok')).strftime("%Y")
     hargabanding = []
     jumlahbanding = []
     idlist = []
