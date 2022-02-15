c0e1850ed143fb0ae58d87503a0dc3e30804906f
rianwp <90314741+rianwp@users.noreply.github.com>
2022-02-15 13:11:26 +0700

    Update __init__.py
    mengganti config init.py

--- website/__init__.py
+++ website/__init__.py
@@ -6,12 +6,19 @@ from flask_login import LoginManager
 
 db = SQLAlchemy()
 
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URL')
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    MAX_CONTENT_LENGTH = 1 * 1024 * 1024
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'rianwp2015'
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-    app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024
+    app.config.from_object(Config)
     
     db.init_app(app)
     
